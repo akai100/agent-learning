3a7b5c40ece43adce112afaa415bf270ce86dad5
Xianglong Zhao <1107971906@qq.com>
2026-01-04 14:33:59 +0800

    Update run.py

--- week1_min_agent/run.py
+++ week1_min_agent/run.py
@@ -1,23 +1,15 @@
-from agent import ReActAgent
+from agent import Agent
+from llm import OpenAILLM
+from tools import ToolRegistry, calculator
+from prompt import SYSTEM_PROMPT
 import openai
 
 openai.api_key = "YOUR_API_KEY"
 
-def llm_call(messages):
-    response = openai.ChatCompletion.create(
-        model="gpt-4o-mini",  # 或你能用的模型
-        messages=messages,
-        temperature=0
-    )
-    return response["choices"][0]["message"]["content"]
+tools = ToolRegistry()
+tools.register("calculator", calculator)
 
+llm = OpenAILLM()
+agent = Agent(llm, tools, SYSTEM_PROMPT)
 
-if __name__ == "__main__":
-    agent = ReActAgent(llm_call)
-
-    task = "What is (12 * 7) + 5?"
-    running = True
-
-    while running:
-        running = agent.step(task)
-        task = ""  # 后续轮次不再重复问题
+agent.run("What is (12 * 7) + 5?")
